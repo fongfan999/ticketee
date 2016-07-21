0f874fa22beb4739a185089f30632fbe20d2b214
Phan Thai Phong <fongfan999@gmail.com>
2016-07-21 10:11:22 +0700

    Projects can now be deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -35,6 +35,12 @@ class ProjectsController < ApplicationController
       flash.now[:alert] = "Project has not been updated."
       render :edit
     end
+  end
+
+  def destroy
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
   end
 
   private
