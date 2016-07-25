2aeeda481eb82ad543724eb6f6bdaf665ae4c3e3
Phan Thai Phong <fongfan999@gmail.com>
2016-07-25 10:12:21 +0700

    Added Devise + sign up feature

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem "font-awesome-rails"
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ spec/features/signing_up_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.feature "Users can sign up" do
+  scenario "when providing valid details" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "test@example.com"
+    fill_in "user_password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+  end
+end
