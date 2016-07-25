070549b7906f49f126dcbb9d7abd8b9081691f25
Phan Thai Phong <fongfan999@gmail.com>
2016-07-25 10:34:59 +0700

    Added sign in and sign out

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -8,5 +8,6 @@ RSpec.feature "Users can sign up" do
     fill_in "user_password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
+    expect(page).to have_content "You have signed up successfully."
   end
 end
