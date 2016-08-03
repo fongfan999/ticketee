fd0e9daaea5306d6abf34366e89e875b3d8eb911
Phan Thai Phong <fongfan999@gmail.com>
2016-08-03 10:28:37 +0700

    Hide the New Ticket link when the user can't create tickets on the project

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -25,6 +25,11 @@ RSpec.feature "Users can only see the appropriate links" do
       visit project_path(project)
       expect(page).not_to have_link "Edit Project"
     end
+
+    scenario "cannot see the New Ticket link" do
+      visit project_path(project)
+      expect(page).not_to have_link "New Ticket"
+    end
   end
 
   context "admin users" do
@@ -43,6 +48,11 @@ RSpec.feature "Users can only see the appropriate links" do
     scenario "can see the Edit Project link" do
       visit project_path(project)
       expect(page).to have_link "Edit Project"
+    end
+
+    scenario "can see the New Ticket link" do
+      visit project_path(project)
+      expect(page).to have_link "New Ticket"
     end
   end
 end
