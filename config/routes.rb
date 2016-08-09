f50bc2c8007467be7c440151c55d78386d00991d
Phan Thai Phong <fongfan999@gmail.com>
2016-08-09 11:00:26 +0700

    Uploaded attachments are now served through the AttachmentsController for extra security

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets, except: [:index]
   end
+
+  resources :attachments, only: "show"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
