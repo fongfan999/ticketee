14aa1db617432157aafba31725e6956d8c81869d
Phong Phan <fongfan999@gmail.com>
2016-09-18 10:27:54 +0700

    Configuring CarrierWave for 12factor

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'devise'
 gem 'pundit', '~> 1.1'
 gem 'carrierwave'
 gem 'searcher', github: 'radar/searcher'
+gem "fog"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,16 @@
 CarrierWave.configure do |config|
   config.root = Rails.root
+
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["S3_RESION"]
+    }
+    config.fog_directory = ENV["S3_BUCKET_NAME"]
+  else
+    config.storage = :file
+  end
 end
