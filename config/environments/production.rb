4f59a752b424bf157480009109deca4ec9fab595
Phong Phan <fongfan999@gmail.com>
2016-09-24 11:08:33 +0700

    Add Mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,23 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Set up Mailgun for sending mail
+  ActionMailer::Base.delivery_method = :smtp
+  host = "ticketee999.herokupapp.com"
+
+  ActionMailer::Base.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: host,
+    authentication: :plain
+  }
+
+  config.action_mailer.default_url_options = {
+    host: host
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
